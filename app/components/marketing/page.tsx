193868be8509bc722924f98b8b835f325c947c15
ra-kesh <iamrakeshkumar@pm.me>
2023-07-15 21:57:22 +0530

    feat:initial routing setup

--- app/components/marketing/page.tsx
+++ app/components/marketing/page.tsx
@@ -3,11 +3,13 @@ import React from "react";
 
 const page = () => {
   return (
-    <div>
-      <h1>marketing</h1>
-      <Link href="/components/marketing/simple-centered">
-        <button>simple centered</button>
-      </Link>
+    <div className="flex-col min-h-screen">
+      <h1 className="text-center py-10 font-bold text-2xl">Marketing</h1>
+      <div className="flex p-5 gap-x-10">
+        <Link href="/components/marketing/simple-centered">
+          <button>simple centered</button>
+        </Link>
+      </div>
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,21 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello, World</h1>;
-      <Link href="/components/marketing">marketing</Link>
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="max-w-2xl">
+        <h1 className="text-4xl font-bold ">Tailwind Components For Fun</h1>
+        <div className="flex justify-evenly my-10">
+          <Link href="/components/marketing">
+            marketing <span>&rarr;</span>
+          </Link>
+          <Link href="/components/application">
+            application <span>&rarr;</span>
+          </Link>
+          <Link href="/components/ecommerce">
+            ecommerce <span>&rarr;</span>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
