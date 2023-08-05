a5db1cb3610729c20ea2e0bbaf8bef3ac3c42665
ra-kesh <iamrakeshkumar@pm.me>
2023-08-05 15:56:36 +0530

    feat;added headless ui and aspect ratio plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,10 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      gridTemplateRows: {
+        "[auto,auto,1fr]": "auto auto 1fr",
       },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/aspect-ratio")],
 };
