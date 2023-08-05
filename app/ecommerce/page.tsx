8f3562a90cf60aa90d3b41ebb461c504cb442147
ra-kesh <iamrakeshkumar@pm.me>
2023-08-05 15:57:11 +0530

    feat;added menu for ecommerce page

--- app/ecommerce/page.tsx
+++ app/ecommerce/page.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import MenuTemplate, { componentDataType } from "../utility/MenuTemplate";
+
+const ecommerceComponents: componentDataType = [
+  {
+    groupId: "1",
+    groupName: "product overviews",
+    groupContents: [
+      {
+        id: 1,
+        name: "product with image grid",
+        path: "ecommerce/with-image-grid",
+      },
+    ],
+  },
+];
 
 const page = () => {
-  return <div>ecommerce</div>;
+  return (
+    <div className="flex-col min-h-screen">
+      <h1 className="text-center py-10 font-bold text-3xl">Application UI</h1>
+      <MenuTemplate componentData={ecommerceComponents} />
+    </div>
+  );
 };
 
 export default page;
